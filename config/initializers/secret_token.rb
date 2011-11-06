320a4a8ec7ae1c9802b08255f51ad7b92621c87b
Eric Downie <cribaceow@Erics-MacBook-Pro.local>
2011-11-06 17:48:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eco1::Application.config.secret_token = 'eb58ef8bb35a912c66d969dd4e4d8e7afa5c3b716ab0a78725b04aa8b370125e673e39a29e912819407189e049c3f400a738d342b8fe96f0e5165974e38da2a9'
