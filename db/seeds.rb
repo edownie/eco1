15bca14e36e8ba939658c747e8c0c617a3742cac
rich <loradownie@rich.local>
2011-11-23 09:13:38 -0500

    run rake db:seed to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+Item.create(:name => "PTG3")
+Item.create(:name => "CMOS")
+Item.create(:name => "EDC18")
+Item.create(:name => "FN51")
+Item.create(:name => "ICA11")
+Item.create(:name => "SMP27")
+Item.create(:name => "ECM20")
+Item.create(:name => "QOC1")
+Item.create(:name => "STR24")
+Item.create(:name => "DLE5")
