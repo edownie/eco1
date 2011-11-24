7bf06d423640c7af8691abaad92054d8daa09546
rich <loradownie@rich.local>
2011-11-24 08:14:19 -0500

    added twitter gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
+  require 'rubygems'
+  require 'twitter'
+  
+  # go here https://github.com/jnunemaker/twitter
+  # to see how the twitter gem works
+  # below is grabbing the first tweet with #hastag (no retweets)
   def index
+    @architecture = Twitter.search("#architecture -rt").first.text
+    @economy = Twitter.search("#economy -rt").first.text
+    @sustainable = Twitter.search("#sustainable -rt").first.text
   end
 
 end
