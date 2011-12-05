0ddae8e4ed25c00e331be165c55c51e5c32f9df0
Eric Downie <cribaceow@Erics-MacBook-Pro.local>
2011-12-05 13:09:03 -0500

    home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,26 @@ class HomeController < ApplicationController
     @architecture = Twitter.search("#architecture -rt").first.text
     @economy = Twitter.search("#economy -rt").first.text
     @sustainable = Twitter.search("#sustainable -rt").first.text
+    @cleanair = Twitter.search("#cleanair -rt").first.text
+    @openinnovation = Twitter.search("#openinnovation -rt").first.text
+    @greenbuilding = Twitter.search("#greenbuilding -rt").first.text
+    @gis = Twitter.search("#gis -rt").first.text
+    @energy = Twitter.search("#energy -rt").first.text
+    @greenrealestate = Twitter.search("#greenrealestate -rt").first.text
+    @crm = Twitter.search("#crm -rt").first.text
+    @construction = Twitter.search("#construction -rt").first.text
+    @solar = Twitter.search("#solar -rt").first.text
+    @biodiversity = Twitter.search("#biodiversity -rt").first.text
+    @bim = Twitter.search("#bim -rt").first.text
+    @cloudcomputing = Twitter.search("#cloudcomputing -rt").first.text
+    @inhabitat = Twitter.search("#inhabitat -rt").first.text
+    @smallbusiness = Twitter.search("#smallbusiness -rt").first.text
+    @aia = Twitter.search("#aia -rt").first.text
+    @applab = Twitter.search("#applab -rt").first.text
+    @adaptivereuse = Twitter.search("#adaptivereuse -rt").first.text
   end
 
+  def _eco_cover
+  end
+  
 end
