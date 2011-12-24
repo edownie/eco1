85a0558ee4fa01974e768829ff4361ca15019233
Rich Downie <rich.downie@livingsocial.com>
2011-12-24 08:14:04 -0500

    created collection buckets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,26 @@ class HomeController < ApplicationController
   require 'twitter'
   
   def index
-    @tweets = Tweet.all
+    # %w{ architecture economy sustainable cleanair openinnovation greenbuilding software incandescent gis energy greenrealestate crm construction solar biodiversity bim cloudcomputing inhabitat smallbusiness aia applab adaptivereuse }
+    @tweets1 = %w{ architecture economy sustainable cleanair openinnovation }
+    @tweets1.collect! do |tweet| 
+      Tweet.find_by_label(tweet)
+    end
+    
+    @tweets2 = %w{ greenbuilding software incandescent gis energy greenrealestate }
+    @tweets2.collect! do |tweet| 
+      Tweet.find_by_label(tweet)
+    end
+    
+    @tweets3 = %w{ crm construction solar biodiversity bim cloudcomputing }
+    @tweets3.collect! do |tweet| 
+      Tweet.find_by_label(tweet)
+    end
+    
+    @tweets4 = %w{ inhabitat smallbusiness aia applab adaptivereuse }
+    @tweets4.collect! do |tweet| 
+      Tweet.find_by_label(tweet)
+    end
   end 
   
   def update_tweets
