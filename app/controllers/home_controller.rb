db4b55080f8903c9029559c194dd8d250bf183fa
Eric Downie <cribaceow@Erics-MacBook-Pro.local>
2012-01-02 14:01:41 -0500

    2files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,22 @@
 class HomeController < ApplicationController
   caches_page :index
   def index
-    @tweets1 = %w{ architecture economy conservation sustainable cleanair leadership openinnovation science environmental building iaq microgrid }
+    @tweets1 = %w{ architecture conservation sustainable cleanair leadership openinnovation science environmental building iaq }
     @tweets1.collect! do |tweet| 
       Tweet.find_by_label(tweet)
     end
     
-    @tweets2 = %w{ greenbuilding software incandescent gis energy greenrealestate }
+    @tweets2 = %w{ greenbuilding software incandescent gis energy greenrealestate economy }
     @tweets2.collect! do |tweet| 
       Tweet.find_by_label(tweet)
     end
     
-    @tweets3 = %w{ crm construction solar biodiversity bim cloudcomputing }
+    @tweets3 = %w{ crm construction solar microgrid biodiversity bim themesh collcons transit}
     @tweets3.collect! do |tweet| 
       Tweet.find_by_label(tweet)
     end
     
-    @tweets4 = %w{ inhabitat smallbusiness aiachat applab adaptivereuse }
+    @tweets4 = %w{ inhabitat smallbusiness aiachat applab adaptivereuse cloudcomputing}
     @tweets4.collect! do |tweet| 
       Tweet.find_by_label(tweet)
     end
