f1c986fd50974ab5980df041ff2031a9b9bd20fc
Rich Downie <rich.downie@livingsocial.com>
2012-01-03 19:54:27 -0500

    fixed

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -44,7 +44,7 @@ class HashtagsController < ApplicationController
 
     respond_to do |format|
       if @hashtag.save
-        format.html { redirect_to @hashtag, notice: 'Hashtag was successfully created.' }
+        format.html { redirect_to hashtags_path, notice: 'Hashtag was successfully created.' }
         format.json { render json: @hashtag, status: :created, location: @hashtag }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class HashtagsController < ApplicationController
 
     respond_to do |format|
       if @hashtag.update_attributes(params[:hashtag])
-        format.html { redirect_to @hashtag, notice: 'Hashtag was successfully updated.' }
+        format.html { redirect_to hashtags_path, notice: 'Hashtag was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
