8497e3b277d351ad3f267313c67eec3d7c247b0d
Rich Downie <rich.downie@livingsocial.com>
2012-02-24 14:25:40 -0500

    started mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,11 @@ private
 def current_user
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
 end
+
+def mobile_device?
+  request.user_agent =~ /Mobile|webOS/
+end
+
 helper_method :current_user
+helper_method :mobile_device?
 end
