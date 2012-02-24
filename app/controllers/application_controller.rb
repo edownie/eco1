8d7827deb46b16306ba8ce955e5874fe9ef8779c
Rich Downie <rich.downie@livingsocial.com>
2012-02-24 14:52:45 -0500

    adding mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,27 @@
 class ApplicationController < ActionController::Base
-protect_from_forgery # See ActionController::RequestForgeryProtection for details
-# force_ssl
+  helper :all
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  before_filter :prepare_for_mobile
 
 private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-def current_user
-  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-end
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
 
-def mobile_device?
-  request.user_agent =~ /Mobile|webOS/
-end
+  helper_method :current_user
+  helper_method :mobile_device?
 
-helper_method :current_user
-helper_method :mobile_device?
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
 end
