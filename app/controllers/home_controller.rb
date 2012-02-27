a8fef305bc492298db9b743dbe6680f258ec5cf9
Rich Downie <rich.downie@livingsocial.com>
2012-02-26 19:59:04 -0500

    cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,7 @@ class HomeController < ApplicationController
     @tweets10 = Tweet.find_all_by_hashtag_id(10)
     @tweets11 = Tweet.find_all_by_hashtag_id(11)
     @tweets12 = Tweet.find_all_by_hashtag_id(12)
+    @all_tweets = Tweet.all
   end
   
   def update_tweets
